607d5706eaadc82529bea9294df0201de3e1bd3f
Madson Santos <madsonsamsung@gmail.com>
2025-05-20 10:24:26 -0300

    refactor: integrate Prisma for user management and authentication, add bcrypt for password hashing

--- back/src/server.ts
+++ back/src/server.ts
@@ -2,20 +2,95 @@ import express, { Request, Response, response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcryptJS";
 import { verifyToken } from "./utils/jwt";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.post("/login", (req, res, next) => {
+app.post("/register", async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400).json({
+      message: "Nome, email e senha são obrigatórios",
+    });
+    return;
+  }
+
   try {
-    const user = {
-      id: 1,
-      name: "Madson Santos",
-      email: "madson@email.com",
-    };
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      res.status(400).json({
+        message: "Usuário já existe",
+      });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        verified: false,
+      },
+    });
+
+    res.status(201).json({
+      message: "Usuário criado com sucesso",
+      user:{
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,        
+      }
+    });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+app.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({
+      message: "Email e senha são obrigatórios",
+    });
+    return;
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      res.status(404).json({
+        message: "Usuário não encontrado",
+      });
+      return;
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      res.status(401).json({
+        message: "Senha E-mail ou senha inválidos",
+      });
+      return;
+    }
+
     const accessToken = jwt.sign(
       {
         id: user.id,
@@ -47,8 +122,12 @@ app.post("/login", (req, res, next) => {
 
     res.status(200).json({
       message: "Usuário autenticado com sucesso",
-      accessToken: accessToken,
-      user,
+      accessToken: accessToken,user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,
+      }
     });
   } catch (error) {
     return next(error);
@@ -56,7 +135,7 @@ app.post("/login", (req, res, next) => {
 });
 
 app.post("/logout", (req, res, next) => {
-  try {     
+  try {
     res.clearCookie("refreshToken");
     res.status(200).json({
       message: "Usuário deslogado com sucesso",
@@ -66,7 +145,7 @@ app.post("/logout", (req, res, next) => {
   }
 });
 
-app.post("/verify-access-token", (req, res, next) => {
+app.post("/verify-access-token", async (req, res, next) => {
   const accessToken = req.headers.authorization?.split(" ")[1];
 
   if (!accessToken) {
@@ -80,6 +159,7 @@ app.post("/verify-access-token", (req, res, next) => {
     accessToken,
     "74590fcd-c273-4569-a684-543139dba664"
   );
+
   if (!decoded?.id) {
     res.status(401).json({
       message: "Unauthorized",
@@ -88,11 +168,17 @@ app.post("/verify-access-token", (req, res, next) => {
   }
 
   try {
-    const user = {
-      id: 1,
-      name: "Madson Santos",
-      email: "madson@email.com",
-    };
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decoded.id,
+      },
+    });
+
+    if (!user) {
+      res.status(404).json({
+        message: "Usuário não encontrado",
+      });
+    }
 
     res.status(200).json({
       message: "Token verificado com sucesso",
@@ -103,22 +189,10 @@ app.post("/verify-access-token", (req, res, next) => {
   }
 });
 
-app.post("/refresh-token", (req, res, next) => {
+app.post("/refresh-token", async (req, res, next) => {
   const refreshToken = req.cookies.refreshToken;
 
-  if (!refreshToken) {    
-    res.clearCookie("refreshToken");
-    res.status(401).json({
-      message: "Unauthorized",
-    });
-    return;
-  }
-
-  const decoded = verifyToken(
-    refreshToken,
-    "4e1f9b17-a768-47dd-b9b8-fc4c9ef00606"
-  );
-  if (!decoded?.id) {    
+  if (!refreshToken) {
     res.clearCookie("refreshToken");
     res.status(401).json({
       message: "Unauthorized",
@@ -127,12 +201,31 @@ app.post("/refresh-token", (req, res, next) => {
   }
 
   try {
-    const user = {
-      id: 1,
-      name: "Madson Santos",
-      email: "madson@email.com",
-      token: "123456789",
-    };
+    const decoded = verifyToken(
+      refreshToken,
+      "4e1f9b17-a768-47dd-b9b8-fc4c9ef00606"
+    );
+
+    if (!decoded?.id) {
+      res.clearCookie("refreshToken");
+      res.status(401).json({
+        message: "Unauthorized",
+      });
+      return;
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decoded.id,
+      },
+    });
+
+    if (!user) {
+      res.status(404).json({
+        message: "Usuário não encontrado",
+      });
+      return;
+    }
 
     const newToken = jwt.sign(
       {
@@ -146,7 +239,7 @@ app.post("/refresh-token", (req, res, next) => {
       }
     );
 
-    res.status(200).json({ 
+    res.status(200).json({
       message: "Token atualizado com sucesso",
       accessToken: newToken,
     });
